0a34e072bf2a6cc963d411fa71dfd939cde6745d
Slavynskiy Pavlo <pavloslav@gmail.com>
2017-08-21 16:59:39 +0300

    Victory over borrow checker

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,12 @@ fn main() {
     arena.fighters.push( Warrior::new(50, Weapon::new("Sword",10,15), "Orc"    ) );
     arena.fighters.push( Warrior::new(60, Weapon::new("Axe",  15,15), "Knight" ) );
     let (f1, f2) = arena.fighters.split_at_mut(1);
-    f1[0].attack( &mut f2[0] );
-    f2[0].attack( &mut f1[0] );
+    {
+        let f1_tgt = f1[0].select_enemy(f2.iter_mut()).unwrap();
+        f1[0].attack( f1_tgt );
+    }
+    {
+        let f2_tgt = f2[0].select_enemy(f1.iter_mut()).unwrap();
+        f2[0].attack( f2_tgt );
+    }
 }
--- src/warrior/warrior.rs
+++ src/warrior/warrior.rs
@@ -2,28 +2,38 @@ use warrior::weapon::Weapon;
 
 pub struct Warrior {
     hp: i32,
+    max_hp: i32,
     weapon: Weapon,
     name: String,
 }
 
 impl Warrior {
+    pub fn new( hp: i32, weapon: Weapon, name: &str) -> Warrior {
+        Warrior {
+            hp: hp,
+            max_hp: hp,
+            weapon: weapon,
+            name: name.to_owned(),
+        }
+    }
     pub fn attack( &mut self, other: &mut Warrior) {
         println!("{} is attacking {} with {}",self.name, other.name, self.weapon.get_name());
         let result = self.weapon.hit(other);
-        println!("{}", result)
+        println!("{}", result);
+        println!("{} now has {}% hp", other.name, other.percent());
     }
     pub fn apply_damage( &mut self, damage: i32 ) -> i32 {
         self.hp -= damage;
         damage
     }
-    pub fn new( hp: i32, weapon: Weapon, name: &str) -> Warrior {
-        Warrior {
-            hp: hp,
-            weapon: weapon,
-            name: name.to_owned(),
-        }
-    }
     pub fn is_alive( &self ) -> bool {
         self.hp != 0
+    }
+    pub fn select_enemy<'a,I>( &self, others: I) -> Option<&'a mut Warrior>
+            where I: Iterator<Item = &'a mut Warrior> {
+        others.min_by_key(|warrior|warrior.hp)
+    }
+    pub fn percent( &self ) -> i32 {
+        (100f64*(self.hp as f64)/(self.max_hp as f64)).round() as i32
     }
 }
